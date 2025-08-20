ab35d3914412a244e8175546f1fd8ee3883c2f89
Ayush Kumar <yeashr007@gmail.com>
2025-08-20 19:52:35 +0530

    vector

--- 01vector.cpp
+++ 01vector.cpp
@@ -3,29 +3,44 @@
 
 using namespace std;
 
+void printArr(vector<int>& arr) {
+    for(int val : arr) {
+        cout << val << " ";
+    }
+}
+
 int main()
 {
     /* SYNTAX */
 
     // vector<int> vec1;
-    // vector<int> vec2(4, 1);
 
-    // vector<char> vec = {'a', 'b' , 'c', 'd', 'e'};
-    // for(char value : vec) {
-    //     cout<< value << " ";
-    // }
+    /*
+    // vector<int> vec2(4, 1);
+    vector<char> vec = {'a', 'b' , 'c', 'd', 'e'};
+    for(char value : vec) {
+        cout<< value << " ";
+    }
+    */
 
 
 
 
     /*
     vector Function:
-    - size
-    - push_back
-    - pop_back
-    - front
-    - back
-    - at
+    - size()
+    - capacity()
+    - push_back()
+    - pop_back()
+    - emplace_back()
+    - front()
+    - back()
+    - at(), []
+
+    - erase()
+    - insert()
+    - clear()
+    - empty()
     */
 
 
@@ -40,7 +55,7 @@ int main()
     vec.push_back(11);
     vec.push_back(62);
 
-    /* size*/
+    /* size() */
     cout << "Size: " << vec.size() << endl;
 
     for (int value : vec)
@@ -49,7 +64,7 @@ int main()
     }
 
 
-    /* pop_back */
+    /* pop_back() */
     vec.pop_back(); // 62
 
     for (int value : vec)
@@ -58,20 +73,84 @@ int main()
     }
     
 
-    /* front */
+    /* front() */
     cout << "First element: " << vec.front() << endl;
 
 
-    /* back */
+    /* back() */
     cout << "last element: " << vec.back() << endl;
 
 
-    /* at */
+    /* at(), [] */
     cout << "Access element: " << vec.at(2) << endl;
+    cout << "Access element: " << vec[2] << endl;
 
-
-    /* capacity */
+    /* capacity() */
     cout << "Capacity: " <<vec.capacity() << endl;
 
-    return 0;
+    vector<int> arr = {1, 2, 3, 4, 5, 6};
+
+    cout << "Array: ";
+    printArr(arr);
+
+    /* erase() */
+    arr.erase(arr.begin());  // 1 -> erase
+    arr.erase(arr.begin() + 2); // 3 -> erase
+
+    cout << "\nErase element: ";
+    printArr(arr);
+
+    /* insert() */
+    arr.insert(arr.begin() + 2, 100);
+
+    cout << "\nInsert element 100 at idx 2 : ";
+    printArr(arr);
+
+    /* clear() */
+    arr.clear();
+    cout<<"\nClear Array: ";
+    printArr(arr);
+    cout << "\nsize of array: " << arr.size() << endl;
+    cout << "capacity of array: " << arr.capacity() << endl;
+
+    /* empty() */
+    cout << "Array is empty or not (i.e., 1 -> True / 0 -> False) : " << arr.empty() << endl;
+
+
+
+    cout << endl
+         << endl;
+
+
+
+    /*  VECTOR ITERATORS */
+
+
+    /*
+        > vec.begin()
+        > vec.end()
+        > vec.rbegin()
+        > vec.rend()
+    */
+
+    vector<int> vec2 = {1, 2, 3, 4, 5, 6};
+
+    /* .begin(), .end()*/
+    vector<int>::iterator it;
+
+    cout << "Iterator begin(), end() : ";
+    for (it = vec2.begin(); it != vec2.end(); it++) {
+        cout << *(it) << " ";
+    }
+
+    /* .rbegin(), .rend()*/
+    cout << "\nReverse Iterator rbegin(), rend() : ";
+
+    // for (vector<int>::iterator it = vec2.begin(); it != vec2.end(); it++) { }
+
+    for (auto it = vec2.rbegin(); it != vec2.rend(); it++) {
+        cout << *(it) << " ";
+    }
+
+        return 0;
 }
